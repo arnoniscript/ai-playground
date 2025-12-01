3da802520b0edc3e45bf2e0cbb26a54ded566044
Luiz Arnoni <luiz.arnoni@gmail.com>
2025-11-30 23:17:13 -0300

    fix: Add jsconfig, npmrc and index files for better module resolution

---
+++ frontend/components/index.ts
@@ -0,0 +1,5 @@
+export { AuthGuard, AdminGuard } from './auth-guard';
+export { Layout } from './layout';
+export { LoginForm } from './login-form';
+export { EvaluationForm } from './evaluation-form';
+export { ModelEmbed } from './model-embed';
